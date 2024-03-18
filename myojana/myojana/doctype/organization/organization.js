0b30f1eb8964cf21fcea545151df087210aa9e73
Abhishek kumar <abhishek.suvaidyam@gmail.com>
2024-03-18 14:14:00 +0530

    add changes in Organization doctypes

--- myojana/myojana/doctype/organization/organization.js
+++ myojana/myojana/doctype/organization/organization.js
@@ -1,8 +1,21 @@
 // Copyright (c) 2024, dhwaniris and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Organization", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Organization", {
+	refresh(frm) {
+        hide_advance_search(frm , ['state','district'])
+        extend_options_length(frm , ['state', 'district'])
+        apply_filter("district", "State", frm, frm.doc.state)
+	},
+    date_of_formation:function(frm){
+        if (new Date(frm.doc.date_of_formation) < new Date(frappe.datetime.get_today())) {
+            frm.doc.date_of_formation = ''
+            frm.set_value("date_of_formation", '')
+            refresh_field('date_of_formation')
+            frappe.throw(__("<b>Date of formation</b> can't be less than today's date"))
+          }
+    },
+    state:function(frm){
+        apply_filter("district", "State", frm, frm.doc.state)
+    }
+});
