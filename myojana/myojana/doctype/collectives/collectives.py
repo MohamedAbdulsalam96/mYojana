327550be8ef064230d882cec30991a905caefe1c
Abhishek kumar <abhishek.suvaidyam@gmail.com>
2024-03-19 16:43:32 +0530

    add reverse addition

--- myojana/myojana/doctype/collectives/collectives.py
+++ myojana/myojana/doctype/collectives/collectives.py
@@ -9,9 +9,16 @@ class Collectives(Document):
 	def validate(self):
 		pass
 
-	
+
 	def on_update(self):
 		if self.get('localname'):
 			return
 		else:
 			self.number_of_members = frappe.db.count("CollectiveMembers", filters={"parent": self.name})
+			# handeling of list of member add two way logics
+			if len(self.list_of_members) > 0:  # Check if the list is not empty
+				for data in self.list_of_members:
+					frappe.db.set_value('Beneficiary Profiling', data.name_of_the_member, 'are_you_a_part_of_collective', 'Yes', update_modified=False)
+					frappe.db.set_value('Beneficiary Profiling', data.name_of_the_member, 'which_collective_are_you_a_part_of', self.name, update_modified=False)
+					print("???????????????????????", data.name_of_the_member)
+	
